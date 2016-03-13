0c5b666f7ff60ef4a02c5c35311231389a0e0c86
Frederik van Genderen <frederik_van_genderen@hotmail.com>
2016-03-13 21:34:35 +0100

    Delete monster.js

--- app/model/monster.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var UserDetail = new mongoose.Schema({
-    username: String,
-    password: String
-}, {collection: 'userInfo'});
-
-module.exports = mongoose.model('userInfo', UserDetail);
